b4a9e3a9b2da39bf59eba52ed7f48f45a931ceff
salmanMohammed2001 <salmannisthar123@gmail.com>
2024-01-19 00:26:11 +0530

    add context

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,14 @@
-
 import './App.css'
+import UserAuth from './context/UserAuth'
 
-import Com_1 from "./components/Com_1";
-import {useState} from "react";
-import {createContext} from "react";
-
-
-// eslint-disable-next-line react-refresh/only-export-components
-export  const  userAuth= createContext({})
 
 
 function App() {
-const [username,setUsername]=useState("")
-
-
-    return(
-       <userAuth.Provider value={{
-        username,
-        setUsername
-       }}>
-           <h1>App {username}</h1>
-            <Com_1 />
-       </userAuth.Provider>
-  )
+    return (
+        <UserAuth>
+            Amila
+        </UserAuth>
+    )
 }
 
 export default App
